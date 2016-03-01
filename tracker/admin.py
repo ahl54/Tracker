0f16e9be9ea05160df8bc8d2b7964622e197ea9a
Lu <lua2@acbc32c2cbf3.chop.edu>
2016-03-01 15:22:01 -0500

    depreciated files removed

--- tracker/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Tracker
-
-# Register your models here.
-
-admin.site.register(Tracker)
-class TrackerAdmin(admin.ModelAdmin):
-    # ...
-    list_display = ('name', 'pub_date', 'trackerID')
